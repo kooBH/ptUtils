dc2134fe9c71c4e566d1d5f6a2c3c635e355c67f
kbh <nine4409@sogang.ac.kr>
2021-03-24 18:02:29 +0900

    MFCC

--- plotting.py
+++ plotting.py
@@ -2,15 +2,28 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pylab as plt
 import numpy as np
-
+from matplotlib import cm
 
 def fig2np(fig):
     data = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep='')
     data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
     return data
 
-def spec2plot(spectrogram):
+def MFCC2plot(MFCC):
+    MFCC = np.transpose(MFCC)
 
+    fig, ax = plt.subplots()
+    im = plt.imshow(MFCC, cmap=cm.jet, aspect='auto')
+    plt.colorbar(im)
+    plt.ylim(-80,20)
+    fig.canvas.draw()
+
+    data = fig2np(fig)
+    plt.close()
+    return data
+
+# Not working
+def spec2plot(spectrogram):
     if len(np.shape(spectrogram)) == 3 :
         spectrogram = np.power(2,spectrogram[:,:,0]) + np.power(2,spectrogram[:,:,1])
         spectrogram = 10*np.log(spectrogram)
--- writer.py
+++ writer.py
@@ -2,8 +2,12 @@ import numpy as np
 import torch
 from tensorboardX import SummaryWriter
 
-from .plotting import spec2plot
+try : 
+    from .plotting import spec2plot,MFCC2plot
+except ImportError:
+    from plotting import spec2plot,MFCC2plot
 
+# https://pytorch.org/docs/stable/tensorboard.html
 
 class MyWriter(SummaryWriter):
     def __init__(self, hp, logdir):
@@ -12,6 +16,8 @@ class MyWriter(SummaryWriter):
         self.window = torch.hann_window(window_length=hp.audio.frame, periodic=True,
                                dtype=None, layout=torch.strided, device=None,
                                requires_grad=False)
+    def log_value(self, train_loss, step,tag):
+        self.add_scalar(tag, train_loss, step)
 
     def log_train(self, train_loss, step):
         self.add_scalar('train_loss', train_loss, step)
@@ -19,11 +25,42 @@ class MyWriter(SummaryWriter):
     def log_test(self,test_loss,step) : 
         self.add_scalar('test_loss', test_loss, step)
 
-    def log_audio(self,noisy,estim,clean,step) : 
+    def log_audio(self,noisy,estim,output,clean,step) : 
         self.add_audio('noisy', noisy, step, self.hp.audio.samplerate)
         self.add_audio('estim', estim, step, self.hp.audio.samplerate)
         self.add_audio('clean', clean, step, self.hp.audio.samplerate)
 
+    def log_MFCC(self,input,output,clean,step):
+        input = input.to('cpu')
+        output = output.to('cpu')
+        clean= clean.to('cpu')
+
+        noisy = input[0]
+        estim = input[1]
+
+        noisy = noisy.detach().numpy()
+        estim = estim.detach().numpy()
+        output = output.detach().numpy()
+        clean= clean.detach().numpy()
+
+        output = np.expand_dims(output,0)
+        clean = np.expand_dims(clean,0)
+
+        noisy = MFCC2plot(noisy)
+        estim = MFCC2plot(estim)
+        output = MFCC2plot(output)
+        clean = MFCC2plot(clean)
+
+        self.add_image('noisy',noisy,step,dataformats='HWC')
+        self.add_image('estim',estim,step,dataformats='HWC')
+        self.add_image('clean',clean,step,dataformats='HWC')
+        self.add_image('output',output,step,dataformats='HWC')
+
+        #self.add_image('noisy',noisy,step)
+        #self.add_image('estim',estim,step)
+        #self.add_image('output',output,step)
+
+    # add_image(tag, img_tensor, global_step=None, walltime=None, dataformats='CHW')
     def log_spec(self,noisy,estim,clean,step) :
         self.add_image('noisy',
             spec2plot(noisy), step, dataformats='HWC')
@@ -43,8 +80,51 @@ class MyWriter(SummaryWriter):
 
         self.log_spec(noisy,estim,clean,step)
 
+def check_MFCC():
+    from hparams import HParam
+    ## log MFCC test
+    hp = HParam("../../config/TEST.yaml")
+    log_dir = '/home/nas/user/kbh/MCFE/'+'/'+'log'+'/'+'TEST'
+
+    writer = MyWriter(hp, log_dir)
+
+    input = torch.load('input.pt').to('cpu')
+    clean = torch.load('clean.pt').to('cpu')
+    output = torch.load('output.pt').to('cpu')
+
+    print('input : ' + str(input.shape))
+    print('output : ' + str(output.shape))
+
+    noisy = input[0]
+    estim = input[1]
+
+    noisy = noisy.detach().numpy()
+    estim = estim.detach().numpy()
+    output = output.detach().numpy()
+    clean= clean.detach().numpy()
+
+
+    output = np.expand_dims(output,0)
+    clean = np.expand_dims(clean,0)
+
+    print(noisy.shape)
+    print(estim.shape)
+    print(output.shape)
+    print(clean.shape)
+
+    noisy = MFCC2plot(noisy)
+    estim = MFCC2plot(estim)
+    output = MFCC2plot(output)
+    clean = MFCC2plot(clean)
+
+    print('MFCC')
+    print(noisy.shape)
+    print(estim.shape)
+    print(output.shape)
+    print(clean.shape)
+
+    writer.log_MFCC(noisy,estim,output,clean,0)
 
 if __name__=='__main__':
-    data = np.load('input.npy')
-    print("input shape : " + str(np.shape(data)))
-    spec2plot(data)
+    check_MFCC()
+
